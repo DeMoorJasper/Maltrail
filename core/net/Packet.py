e188dec9476d1ecc8ae3e929dd2ecff20c23ba12
DeMoorJasper <jasperdemoor@gmail.com>
2018-10-21 18:55:35 +0200

    use impacket, TODO: update plugins & remove deprecated Packet fields

--- core/net/Packet.py
+++ core/net/Packet.py
@@ -4,35 +4,30 @@ import socket
 from core.settings import LOCALHOST_IP
 from core.settings import IPPROTO_LUT
 from core.enums import PROTO
-from core.net.addr import inet_ntoa6
+
+from impacket.ImpactDecoder import IPDecoder
 
 class Packet(object):
     src_port = "-"
     dst_port = "-"
-    proto = None # Protocol name ex. TCP
+    proto = None  # Protocol name ex. TCP
     is_empty = False
+    decoder = IPDecoder()
 
     def __init__(self, packet, sec, usec, ip_offset):
         self.sec = sec
         self.usec = usec
-        
-        self.ip_data = packet[ip_offset:]
-        self.ip_version = ord(self.ip_data[0]) >> 4
-        self.localhost_ip = LOCALHOST_IP[self.ip_version]
+        ip_data = packet[ip_offset:]
+        self.ip = self.decoder.decode(ip_data)  # Parsed IP Packet
 
-        if self.ip_version == 0x04:  # IPv4
-            self.ip_header = struct.unpack("!BBHHHBBH4s4s", self.ip_data[:20])
-            self.iph_length = (self.ip_header[0] & 0xf) << 2
-            self.protocol = self.ip_header[6]
-            self.src_ip = socket.inet_ntoa(self.ip_header[8])
-            self.dst_ip = socket.inet_ntoa(self.ip_header[9])
-        elif self.ip_version == 0x06:  # IPv6
-            # Reference: http://chrisgrundemann.com/index.php/2012/introducing-ipv6-understanding-ipv6-addresses/
-            self.ip_header = struct.unpack("!BBHHBB16s16s", self.ip_data[:40])
-            self.iph_length = 40
-            self.protocol = self.ip_header[4]
-            self.src_ip = inet_ntoa6(self.ip_header[6])
-            self.dst_ip = inet_ntoa6(self.ip_header[7])
+        # Everything below this comment is deprecated!
+        self.ip_data = ip_data
+        self.ip_version = self.ip.get_ip_v()
+        self.localhost_ip = LOCALHOST_IP[self.ip_version]
+        self.src_ip = self.ip.get_ip_src()
+        self.dst_ip = self.ip.get_ip_dst()
+        self.iph_length = self.ip.get_header_size()
+        self.protocol = self.ip.get_ip_p()
 
         if self.protocol == socket.IPPROTO_TCP:
             self.proto = PROTO.TCP
